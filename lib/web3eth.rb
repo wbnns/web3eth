ee4c03a67822b6d88c3943f9921c52b28796bb0d
Will Binns <hello@willbinns.org>
2021-04-06 19:46:12 +0200

    Initialize repository

---
+++ lib/web3eth.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "web3eth/version"
+
+module Web3eth
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/web3eth_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Web3eth do
+  it "has a version number" do
+    expect(Web3eth::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
